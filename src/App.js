3eb5584b04dea0fd99bbd588b7558cf930a3158a
UgurcanSARICI <ugurcansarici@gmail.com>
2022-10-07 21:52:42 +0300

    media

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./scss/App.scss";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
+import Card from "./components/Card";
+import data from "./data.js";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Header />
+      <Card data={data} />
     </div>
   );
 }
